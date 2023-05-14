832fffea186e309e59d49aeeb0bdf76dace2fd57
Tamerlan75 <125548161+Tamerlan75@users.noreply.github.com>
2023-05-14 22:48:40 +0300

    Add files via upload

---
+++ Final work/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string[] array = new string[] {"geek", "brains", "it", "!"};
+Console.Write( array[0]+ " " );
+Console.Write( array[1]+ " " );
+Console.Write( array[2] );
+Console.Write( array[3]+ " " );
+Console.Write ("=>"+ " ");
+
+void ChangeArray (string[] array )
+{
+  string[] newArray = new string[array.Length];
+  int Index = 0;
+  int newIndex =0;
+for ( Index = 0; Index < array.Length; Index++)
+ {
+     if(array[Index].Length<=3)
+          newArray[newIndex]= array[Index];
+     Console.Write( newArray[newIndex] );     
+ }
+}
+ChangeArray(array);
